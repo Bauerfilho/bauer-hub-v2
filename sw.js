75096c7ba7c8d36c1413c037932b46a6b6ea4fa3
Bauerfilho <bauervieiracesar@icloud.com>
2026-09-26 00:11:54 -0300

    Publicar PWA da fonte 0d9348168cd27c9998f4aff5c3ef4267797e88c0

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* Atualização autoral: uma edição só fica utilizável depois da conferência completa. */
 'use strict';
-const VERSION = '8f5f6644ede2274cdb50660cc2f6f5987deab296';
-const MANIFEST_HASH = '3aa2973357b188ecd0aa9399e21bbc8ac5691328394ecbb6576a1f29651228ed';
+const VERSION = '0d9348168cd27c9998f4aff5c3ef4267797e88c0';
+const MANIFEST_HASH = 'c1f76162e409cc1ea0da89d5acf87a69b62c8fac2df6fe1ca9aa16d4b71d0e4f';
 const BASE = new URL(self.registration.scope);
 const PREFIX = 'orq-receituarios:' + encodeURIComponent(BASE.pathname) + ':';
 const CACHE = PREFIX + VERSION;
